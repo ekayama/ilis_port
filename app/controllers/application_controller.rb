affce0a60dbc4aa615b60959914320714147cc4b
EC2 Default User <ec2-user@ip-172-31-42-98.ap-northeast-1.compute.internal>
2024-12-26 19:42:46 +0000

    [save] ilis__port

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  layout :set_layout
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email])
+  end
+  
+  def set_layout 
+    if user_signed_in? && current_user.admin? 
+      "admin" 
+    elsif user_signed_in? 
+      "user" 
+    else 
+      "application" 
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    # ↓ローカルに追加されたコントローラーを参照する(コントローラー名: "コントローラーの参照先")
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
+  
+  devise_for :admins, controllers: {
+    # ↓ローカルに追加されたコントローラーを参照する(コントローラー名: "コントローラーの参照先")
+    registrations: "admins/registrations",
+    sessions: "admins/sessions",
+    passwords: "admins/passwords",
+    confirmations: "admins/confirmations"
+  }
+
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
